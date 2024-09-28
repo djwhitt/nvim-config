126ff067db25234d39d24b61175b62151ba8d130
David Whittington <djwhitt@gmail.com>
2024-09-28 12:54:05 -0500

    Setup copilot-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,12 +1,72 @@
-  return {
-    'hrsh7th/nvim-cmp',
-    opts = function(_, opts)
-      local cmp = require('cmp')
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+    local luasnip = require("luasnip")
 
-      -- Avoid interfering with GitHub Copilot
-      opts.mapping['<Tab>'] = cmp.config.disable
-      opts.mapping['<S-Tab>'] = cmp.config.disable
+    -- Adjust <Tab> behavior as recommended by copilot-cmp
+    local has_words_before = function()
+      if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0
+        and vim.api
+            .nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]
+            :match("^%s*$")
+          == nil
+    end
+    opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_itemh()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end)
 
-      return opts
-    end,
-  }
+    -- copilot-cmp recommended config
+    opts.sources = cmp.config.sources {
+      { name = "copilot", group_index = 2 },
+      { name = "nvim_lsp", group_index = 2 },
+      { name = "luasnip", group_index = 2 },
+      --{ name = "buffer", group_index = 2 },
+      { name = "path", group_index = 2 },
+    }
+
+    -- copilot-cmp recommended config
+    opts.formatting = {
+      format = lspkind.cmp_format {
+        mode = "symbol",
+        max_width = 50,
+        symbol_map = { Copilot = "" },
+      },
+    }
+
+    -- copilot-cmp recommended config
+    opts.sorting = {
+      priority_weight = 2,
+      comparators = {
+        require("copilot_cmp.comparators").prioritize,
+
+        -- Below is the default comparitor list and order for nvim-cmp
+        cmp.config.compare.offset,
+        -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    }
+
+    return opts
+  end,
+}
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,18 @@
 return {
-  "github/copilot.vim",
-  event = "VeryLazy",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        ["*"] = false,
+        clojure = true,
+        javascipt = true,
+        lua = true,
+        typescript = true,
+      },
+    }
+  end,
 }
