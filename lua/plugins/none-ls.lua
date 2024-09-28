d2d93a987ec2dec59dc08504b901890b46f6d576
David Whittington <djwhitt@gmail.com>
2024-09-28 12:54:34 -0500

    Enable none-ls prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -12,11 +12,9 @@ return {
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- TODO enable prettier and eslint with restrictions
-      --null_ls.builtins.formatting.eslint,
-      --null_ls.builtins.formatting.prettier,
       null_ls.builtins.diagnostics.selene,
       null_ls.builtins.formatting.nixfmt,
+      null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.stylua,
     }
     return config -- return final config table
