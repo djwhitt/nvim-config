d7f1c5f7046dfb3b907bf1aa6af7febf58b6fa77
David Whittington <djwhitt@gmail.com>
2024-10-12 09:47:33 -0500

    Disable ts_ls formatting
    I use Biome and Prettier instead

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -30,7 +30,7 @@ return {
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         "lua_ls",
-        "tsserver",
+        "ts_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
