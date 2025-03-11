21909c07c9264e9d228a52dd105b9af510e80624
David Whittington <djwhitt@gmail.com>
2025-03-10 19:29:31 -0500

    Enable buffer tabs (for aider) + start aider to watch files by default

--- lua/plugins/aider.lua
+++ lua/plugins/aider.lua
@@ -3,7 +3,12 @@ return {
     "joshuavial/aider.nvim",
     lazy = false,
     opts = {
-      auto_manage_context = false,
+      --auto_manage_context = false,
+      default_bindings = false,
     },
+    init = function()
+      vim.api.nvim_set_keymap('n', '<leader>Ao', ':AiderOpen --watch-files<CR>', {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', '<leader>Am', ':AiderAddModifiedFiles<CR>', {noremap = true, silent = true})
+    end
   },
 }
