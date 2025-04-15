b0dc6ff2f7868eedc1e408c42e3d99b7c1032fca
David Whittington <djwhitt@gmail.com>
2025-04-15 18:56:07 -0500

    Tring a different aider plugin

--- lua/plugins/aider.lua
+++ lua/plugins/aider.lua
@@ -1,14 +1,40 @@
 return {
-  {
-    "joshuavial/aider.nvim",
-    lazy = false,
-    opts = {
-      --auto_manage_context = false,
-      default_bindings = false,
+  "GeorgesAlkhouri/nvim-aider",
+  cmd = "Aider",
+  -- Example key mappings for common actions:
+  keys = {
+    { "<leader>a/", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
+    {
+      "<leader>as",
+      "<cmd>Aider send<cr>",
+      desc = "Send to Aider",
+      mode = { "n", "v" },
     },
-    init = function()
-      vim.api.nvim_set_keymap('n', '<leader>Ao', ':AiderOpen --watch-files<CR>', {noremap = true, silent = true})
-      vim.api.nvim_set_keymap('n', '<leader>Am', ':AiderAddModifiedFiles<CR>', {noremap = true, silent = true})
-    end
+    { "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
+    { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
+    { "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
+    { "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
+    { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
   },
+  dependencies = {
+    "folke/snacks.nvim",
+    --- The below dependencies are optional
+    "catppuccin/nvim",
+    "nvim-tree/nvim-tree.lua",
+    --- Neo-tree integration
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = function(_, opts)
+        -- Example mapping configuration (already set by default)
+        opts.window = {
+          mappings = {
+            ["+"] = { "nvim_aider_add", desc = "add to aider" },
+            ["-"] = { "nvim_aider_drop", desc = "drop from aider" }
+          }
+        }
+        require("nvim_aider.neo_tree").setup(opts)
+      end,
+    },
+  },
+  config = true,
 }
