0f573052800b1ce8b6e406a912634ea02796ca90
David Whittington <djwhitt@gmail.com>
2025-04-26 16:24:03 -0500

    Tweaking vimwiki setup

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,6 +2,9 @@ return {
   "vimwiki/vimwiki",
   lazy = false,
   init = function()
-    vim.g["vimwiki_list"] = { { path = "~/Vimwiki/" } }
+    vim.g["vimwiki_global_ext"] = 0;
+    vim.g["vimwiki_list"] = {
+      { path = "~/Vimwiki/", syntax = 'markdown', ext = 'md' },
+    }
   end,
 }
